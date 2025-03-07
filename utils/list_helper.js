a1dfb1656f23c0a3c54e72232fb2d777856c921f
WilmarDeML <carewaz23@hotmail.com>
2025-03-06 19:51:56 -0500

    solve exercise 4.5*

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,13 @@ const dummy = (blogs) => 1
 
 const totalLikes = blogs => blogs.reduce((acc, blog) => acc + blog.likes, 0)
 
+const favoriteBlog = blogs => blogs.reduce((ant, act) => {
+  const { title, author, likes } = ant.likes > act.likes ? ant : act
+  return { title, author, likes }
+}, {})
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
