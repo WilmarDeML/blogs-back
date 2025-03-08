0625ed038ef405d32182cd376b789bdad532764b
WilmarDeML <carewaz23@hotmail.com>
2025-03-08 15:28:41 -0500

    solve exercise 4.18

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const { MONGODB_URI } = require('./utils/config')
@@ -25,6 +26,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
